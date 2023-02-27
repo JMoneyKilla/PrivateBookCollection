7ad152211550788064569f003b13b3fcd60c079f
Madsp15 <madsp@hotmail.dk>
2023-02-27 10:01:32 +0100

    update database

--- BookCollection/src/dal/DAO.java
+++ BookCollection/src/dal/DAO.java
@@ -30,11 +30,33 @@ public class DAO {
     }
     private void deleteBook(Book book) {
         int id = book.getId();
-        String sql ="DELETE FROM Songs WHERE id=(id) (VALUES(?))";
+        String sql ="DELETE FROM Songs WHERE id=?";
 
         try(Connection con = dbc.getConnection();) {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1,id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void updateBook(Book book){
+        int id = book.getId();
+        String title = book.getTitle();
+        int author = book.getAuthor();
+        int genre = book.getGenre();
+        String lastRead = book.getLastRead();
+
+
+        String sql = "UPDATE Songs SET Title = ?, author = ?, genre = ?, lastRead = ? WHERE id = ?;";
+        try(Connection con = dbc.getConnection();) {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1,title);
+            ps.setInt(2,author);
+            ps.setInt(3,genre);
+            ps.setString(4,lastRead);
+            ps.setInt(5,id);
+            ps.execute();
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
